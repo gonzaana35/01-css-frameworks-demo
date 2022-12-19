580759ebcfc9a3e9a0ec7ebcb7107e1a600c657f
Kat Law <kathleen.law@multiverse.io>
2022-12-19 16:36:12 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+function App() {
+    return (
+        <Container>
+            <h1>hello, world</h1>
+        </Container>
+    );
+}
+
+export { App };
