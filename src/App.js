dfffc3c957995b44d4e6a578b37c49e417f18bfa
Ana Gonzalez Hiraldo <gonzaana35@gmail.com>
2023-01-03 09:59:26 -0500

    imported Stack component into App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import Stack from 'react-bootstrap/Stack'
 
 function App() {
     return (
         <Container>
             <h1>hello, world</h1>
+            <stack></stack>
         </Container>
     );
 }
